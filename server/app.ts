b0694c09fdd7584b727e59578af88c764f9c2c32
emilcall <163155784+emilcall@users.noreply.github.com>
2025-03-06 15:28:42 +0100

    update v3

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,29 @@
-import express, { Application, Request, Response } from "express";
-import routes from "./routes";
+import express, { Application } from "express";
 import path from "path";
+import expressLayouts from "express-ejs-layouts";
+import routes from "./routes";
 
 const app: Application = express();
-const PORT: number = 3000;
+const PORT: number = 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// EJS als template-engine instellen
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
+// Middleware voor layouts
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
+
+// Middleware om statische bestanden te serveren
 app.use(express.static(path.join(__dirname, "/public")));
 
+// Middleware om formulierdata te verwerken
+app.use(express.urlencoded({ extended: true }));
+
+// Routes gebruiken
 app.use("/", routes);
 
-app.listen(PORT, () => {
+// Server starten
+app.listen(PORT, (): void => {
   console.log(`Server draait op http://localhost:${PORT}`);
 });
--- server/routes.ts
+++ server/routes.ts
@@ -1,20 +1,19 @@
 import express, { Request, Response } from "express";
 import { getNews, getNewsBySlug, addNews } from "./data/newsService";
-import path from "path";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response): void => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
-  });
 
-  router.get("/", (req: Request, res: Response): void => {
-    const news = getNews();
-    res.render("index", { title: "Nieuws", news });
-  });
+router.get("/", (req: Request, res: Response): void => {
+  const news = getNews();
+  console.log(news)
+  res.render("index", { title: "Nieuws", news });
+});
 
 router.get("/news/:slug", (req: Request, res: Response): void => {
     const { slug } = req.params;
-    const artical = getNewsBySlug(slug);
+    const article = getNewsBySlug(slug);
+    res.render("news", { article, title: "Nieuwsdetail"})
 })
+
 export default router;
